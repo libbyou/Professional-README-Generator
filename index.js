71589349589f5a6e950fc279c65b74e5add71d50
libbyou <lcyou95@gmail.com>
2023-10-19 15:16:47 -0500

    edits made to the index.js and generateMarkdown file

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const GenerateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What instructions do you have for installation?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license are you using for your project',
+        choices: [, 'GNU', 'Apache', 'MIT', 'Mozilla', 'Creative Commons', 'None'],
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile('./utils/README.md', GenerateMarkdown(data), (err) =>
+    err ? console.log(err) : console.log('README made!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        writeToFile(data);
+    });
+    }
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,46 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![Github License](https://img.shields.io/badge/License-${license}-yellow.svg)`;
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `![Github License](https://choosealicense.com/licenses/${license}/)`;
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `This is licensed under ${license}`
+  } else {
+    ``;
+  }
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${data.description}
+  ${data.installation}
+  ${data.usage}
+  
+  ## License
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseLink(data.license)}
 
 `;
 }
